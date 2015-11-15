5dc30a517666381b0fe1bf32fae8d155d75fea20
Eugene Malinin <malinin13@gmail.com>
2015-11-15 18:13:56 +0100

    fixes

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $('body').mousemove(function (e) {
-        var amountMovedX = (e.pageX * -1 / 6);
-        var amountMovedY = (e.pageY * -1 / 6);
+        var amountMovedX = (e.pageX/2 * -1 / 5);
+        var amountMovedY = (e.pageY/2 * -1 / 5);
         if(amountMovedY >= -250) {
             $('.hero-section-overlay').css({'transform': 'translate3d(' + amountMovedX + 'px,' + amountMovedY + 'px, 0px)'});
         }
