770aa7f1ee56e506d12af48b29c8b6f0c68a3433
Eugene Malinin <malinin13@gmail.com>
2015-11-16 10:30:42 +0100

    top arrow fixed

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -12,15 +12,18 @@ $(document).ready(function () {
         dots: true
     });
 
+    var topArrow = $('#return-to-top');
+
     // ===== Scroll to Top ====
     $(window).scroll(function () {
-        if ($(this).scrollTop() >= 350) {        // If page is scrolled more than 50px
-            $('#return-to-top').fadeIn(200);    // Fade in the arrow
+        if ($(window).scrollTop() >= 550) {        // If page is scrolled more than 50px
+            topArrow.fadeIn(200);    // Fade in the arrow
         } else {
-            $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+            topArrow.fadeOut(200);   // Else fade out the arrow
         }
     });
-    $('#return-to-top').click(function () {      // When arrow is clicked
+
+    topArrow.click(function () {      // When arrow is clicked
         $('body,html').animate({
             scrollTop: 0                       // Scroll to top of body
         }, 500);
